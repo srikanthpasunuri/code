1aa4f57b5e8031607eb4edc007553395de1b606a
raghubabukappala <46133701+raghubabukappala@users.noreply.github.com>
2021-12-21 22:16:08 +0530

    Update ubuntu.tf

--- ubuntu.tf
+++ ubuntu.tf
@@ -6,5 +6,5 @@ provider "aws" {
 resource "aws_instance" "mac_Ubuntu2" {
   ami           = "ami-04505e74c0741db8d"
   instance_type = "t2.micro"
-  key_name      = "forboth"
+  key_name      = "Navya"
 }
